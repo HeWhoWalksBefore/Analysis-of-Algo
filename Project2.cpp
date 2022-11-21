b797ecd46622b8aad7cba54932f5ced54fd65f86
HeWhoWalksBefore <dviera@usf.edu>
2022-11-20 19:29:51 -0500

    Inserted comments

--- Project2.cpp
+++ Project2.cpp
@@ -7,29 +7,35 @@ using namespace std;
 
 int main()
 {
+    //Open input file as omni & test for success
     ifstream omni;
     omni.open ("small-omni-input.txt");
     if (omni.is_open())
         cout << "success" << endl;
     else
         cout << "failure";
+    
+    //get to line 4 (where it tells us part num and operation num)
     string current;
     int i = 0;
     for (int i = 0; i < 2; i++){
         getline(omni, current);
-        cout << current << endl;
+        //cout << current << endl;      /*displays content*/
     }
-    getline(omni, current);
-    cout << "Robot Type: " << current << endl;
+    getline(omni, current);             /*gets line 3 (name of robot)*/
+    //cout << "Robot Type: " << current << endl;        //displays name of robot
 
 
+    //create queue for the list of operations, and read the number of operations and parts into variables
     queue<pair<int, int>> operations_list;
     int numParts, numOperations = 0;
     omni >> numParts;
     omni >> numOperations;
-    cout << numParts << " parts, " << numOperations << " operations." << endl;
     getline(omni, current);
+    //cout << numParts << " parts, " << numOperations << " operations." << endl; /*display part and operation num*/
 
+
+    //store the operations in the queue "operation"   
     int addedPart, createdPart;
     pair<int, int> operation;
     for (i = 0; i < numOperations; i++){
@@ -39,15 +45,22 @@ int main()
         operations_list.push(operation);
     }
 
+    //store the part values in vector "partValues"
     vector<int> partValues;
     int cost;
     for (i = 0; i < numParts; i++){
         omni >> cost;
         partValues.push_back(cost);
     }
-    cout << "size" << partValues.size() << endl;
+
+
+    //display part values for testing
+    cout << "size " << partValues.size() << endl;
     for (i = 0; i < partValues.size(); i++){
         cout << partValues.at(i) << endl;
     }
+
+    
+
     return 0;
 }
